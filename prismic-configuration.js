423ef8619df111f976ffe861fea3bd00d489fe58
Phil Snow <phil.snow@prismic.io>
2021-10-12 17:50:29 +0200

    test

--- prismic-configuration.js
+++ prismic-configuration.js
@@ -1,12 +1,15 @@
 // -- Prismic API endpoint
-export const apiEndpoint = "https://lamesarv.cdn.prismic.io/api/v2";
+export const apiEndpoint = "https://lamesarv-phil.cdn.prismic.io/api/v2";
 // -- Access Token if the repository is not public
 export const accessToken = "";
 
-export const repoName = "lamesarv";
+export const repoName = "lamesarv-phil";
 
 // -- Link resolution rules
 export const linkResolver = (doc) => {
+  if (doc.uid === "home") {
+    return `/`;
+  }
   if (doc.type === "landing_page") {
     return `/${doc.uid}`;
   }
